a4caa456cec8859c2872687cff662387e31c0a75
Reza Pourmohammadi <pourmohammadi2001@gmail.com>
2023-09-28 14:47:30 +0330

    Decoder added

---
+++ Decoder.py
@@ -0,0 +1,143 @@
+doubleFrequency = {"no": 1.9607843137254901, "ot": 0.8714596949891068, "th": 2.7233115468409586, "hi": 1.3071895424836601, "in": 2.178649237472767, "ng": 1.5250544662309369, "ge": 0.8714596949891068, "el": 1.3071895424836601, "ls": 0.8714596949891068, "se": 1.8518518518518516, "em": 1.1982570806100217, "ma": 1.1982570806100217, "at": 1.9607843137254901, "tt": 1.9607843137254901, "te": 1.1982570806100217, "er": 3.0501089324618738, "rs": 1.0893246187363834, "ss": 0.32679738562091504, "so": 0.8714596949891068, "on": 0.7625272331154684, "gb": 0.10893246187363835, "by": 0.10893246187363835, "ym": 0.10893246187363835, "me": 0.4357298474945534, "et": 0.32679738562091504, "ta": 0.10893246187363835, "al": 0.32679738562091504, "ll": 0.32679738562091504, "li": 0.5446623093681917, "ic": 0.10893246187363835, "ca": 0.9803921568627451, "as": 0.10893246187363835, "oc": 0.32679738562091504, "cl": 0.32679738562091504, "lo": 0.32679738562091504, "os": 0.32679738562091504, "en": 1.0893246187363834, "om": 0.8714596949891068, "rh": 0.32679738562091504, "ho": 0.6535947712418301, "ow": 1.3071895424836601, "wf": 0.32679738562091504, "fa": 0.32679738562091504, "ar": 1.8518518518518516, "rc": 1.0893246187363834, "co": 0.32679738562091504, "ou": 0.9803921568627451, "ul": 0.32679738562091504, "ld": 0.32679738562091504, "dn": 0.9803921568627451, "nt": 0.7625272331154684, "tb": 0.32679738562091504, "be": 0.32679738562091504, "mu": 0.32679738562091504, "uc": 0.32679738562091504, "ch": 0.32679738562091504, "hm": 0.32679738562091504, "mo": 0.32679738562091504, "or": 2.178649237472767, "re": 2.0697167755991286, "ef": 0.32679738562091504, "fr": 0.32679738562091504, "ro": 0.5446623093681917, "mt": 0.32679738562091504, "he": 1.7429193899782136, "eh": 0.32679738562091504, "ea": 1.0893246187363834, "rt": 0.6535947712418301, "tf": 0.32679738562091504, "fo": 1.6339869281045754, "ev": 1.6339869281045754, "ve": 1.8518518518518516, "tr": 0.5446623093681917, "ru": 0.7625272331154684, "us": 0.9803921568627451, "st": 1.0893246187363834, "ti": 0.7625272331154684, "gw": 0.32679738562091504, "wh": 1.0893246187363834, "we": 0.5446623093681917, "an": 0.9803921568627451, "nd": 1.4161220043572984, "sn": 0.4357298474945534, "ne": 1.5250544662309369, "op": 0.4357298474945534, "pe": 0.4357298474945534, "ed": 1.0893246187363834, "dm": 0.2178649237472767, "my": 0.2178649237472767, "ys": 0.32679738562091504, "lf": 0.2178649237472767, "ft": 0.2178649237472767, "is": 0.6535947712418301, "sw": 0.4357298474945534, "wa": 0.7625272331154684, "ay": 1.0893246187363834, "yl": 0.2178649237472767, "if": 0.6535947712418301, "fe": 0.4357298474945534, "ei": 0.4357298474945534, "ur": 0.4357298474945534, "iv": 0.2178649237472767, "it": 0.32679738562091504, "to": 0.2178649237472767, "rw": 0.9803921568627451, "ya": 0.4357298474945534, "lt": 0.2178649237472767, "es": 0.32679738562091504, "ew": 0.6535947712418301, "wo": 0.4357298474945534, "rd": 0.2178649237472767, "ds": 0.2178649237472767, "si": 0.2178649237472767, "id": 0.2178649237472767, "do": 0.5446623093681917, "tj": 0.2178649237472767, "ju": 0.2178649237472767, "ts": 0.2178649237472767, "sa": 0.32679738562091504, "ee": 0.2178649237472767, "ek": 0.2178649237472767, "ka": 0.2178649237472767, "di": 0.7625272331154684, "fi": 0.2178649237472767, "ny": 0.2178649237472767, "yo": 0.2178649237472767, "ue": 0.2178649237472767, "ry": 0.2178649237472767, "yd": 0.5446623093681917, "da": 0.2178649237472767, "yf": 0.2178649237472767, "gn": 0.2178649237472767, "nm": 0.2178649237472767, "mi": 0.2178649237472767, "df": 1.0893246187363834, "ra": 0.2178649237472767, "ad": 0.2178649237472767, "ff": 0.2178649237472767, "tv": 0.2178649237472767, "vi": 0.2178649237472767, "ie": 0.2178649237472767, "ha": 0.7625272331154684, "ey": 0.8714596949891068, "yk": 0.32679738562091504, "kn": 0.6535947712418301, "wb": 0.2178649237472767, "bu": 0.2178649237472767, "ut": 0.2178649237472767, "ik": 0.32679738562091504, "ws": 0.10893246187363835, "ri": 0.10893246187363835, "tc": 0.10893246187363835, "wi": 0.10893246187363835, "yn": 0.2178649237472767, "rg": 0.10893246187363835, "ga": 0.10893246187363835, "am": 0.10893246187363835, "yp": 0.10893246187363835, "pl": 0.10893246187363835, "la": 0.10893246187363835, "wy": 0.10893246187363835, "ye": 0.2178649237472767, "ah": 0.2178649237472767, "hy": 0.10893246187363835, "hs": 0.10893246187363835}
+
+def printer(mat):
+        for i in mat:
+            print(i)
+        print("*"*40,end='\n\n')
+        
+CypherText = input()
+CypherText = CypherText.lower()
+
+soleFrequency = [0]*26
+
+for i in range(len(CypherText)):
+    if CypherText[i] != '*':
+        soleFrequency[ord(CypherText[i])-97] += 1
+
+for i in range(26):
+    soleFrequency[i] = soleFrequency[i]/(len(CypherText)-1)
+
+score_table = []
+for i in range(26):
+    score_table.append([0]*26)
+
+for i in range(26):
+    for j in range(26):
+        final_score = 0
+        double_letters = chr(i+97) + chr(j+97)
+        if double_letters in doubleFrequency:
+            score = (doubleFrequency[double_letters]/100)/(soleFrequency[i]*soleFrequency[j])
+            if score > 1:
+                final_score = int((score/1.7))
+                if score > 8:
+                    final_score = 8
+            else:
+                score = (soleFrequency[i]*soleFrequency[j])/(doubleFrequency[double_letters]/100)
+                final_score = int(-(score/1.7))
+                if score < -8:
+                    final_score = -8
+        else:
+            final_score = -8
+        score_table[i][j] = final_score
+            
+
+validkey = 1
+        
+for key in range(1,len(CypherText)+1):
+    if len(CypherText)%key == 0:
+        rows = int(len(CypherText)/key)
+        columns = key
+        matrix = []
+        for i in range(rows):
+            matrix.append([])
+            
+        counter = 0
+        for i in range(columns):
+            for j in range(rows):
+                matrix[j].append(CypherText[counter])
+                counter += 1
+        
+        if key == 1:
+            pass
+        else:
+            mat = []
+            for i in range(columns):
+                mat.append([])
+                
+            for i in range(columns):
+                for j in range(columns):
+                    if i == j:
+                        mat[i].append(-8)
+                    else:
+                        sum = 0
+                        for z in range(rows):
+                            s = matrix[z][i] + matrix[z][j]
+                            if s in doubleFrequency:
+                                sum += doubleFrequency[s]*score_table[ord(matrix[z][i])-97][ord(matrix[z][j])-97]
+                            else:
+                                sum += -8
+                        mat[i].append(int((sum/rows)*1000)/1000)
+
+            negativeMaxCounter = 0
+            visited = set()
+            flag = False
+            flag2 = False
+            for i in range(columns):
+                max = -16
+                jmax = 0 
+                for j in range(columns):
+                    if mat[i][j] > max:
+                        max = mat[i][j]
+                        jmax = j
+                
+                if max < 0:
+                    negativeMaxCounter += 1
+                    
+                if jmax not in visited:
+                    visited.add(jmax)
+                elif jmax in visited and not flag:
+                    flag = True
+                else:
+                    flag2 = True
+                    break
+            if key == 184:
+                print(flag2)
+            if negativeMaxCounter <= 1:
+                validkey = key
+                dict = {}
+                columnsList = []
+                for i in range(columns):
+                    columnsList.append(i)
+                for i in range(columns):
+                    max = -16
+                    jmax = 0 
+                    for j in range(columns):
+                        if mat[i][j] > max:
+                            max = mat[i][j]
+                            jmax = j
+                    if max > 0:
+                        dict[i] = jmax
+                        columnsList.remove(jmax)
+                firstcolumn = columnsList[0]
+                keyList = [firstcolumn]
+                x = firstcolumn
+                for i in range(key-1):
+                    keyList.append(dict[x])
+                    x = dict[x]
+                    
+                s = ""
+                flag3 = True
+                for i in range(rows):
+                    for j in keyList:
+                        if matrix[i][j] != '*':     
+                            s += matrix[i][j]
+                        else:
+                            flag3 = False
+                            break
+                    if not flag3:
+                        break
+                            
+                print(key)
+                print(*keyList)
+                print(s)
+                exit()
